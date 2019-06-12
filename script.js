0b069862fe140e038775bde5ad382340776aabb9
CN0075 <cn0075@CN0075.lan>
2019-06-12 15:59:49 +0100

    complete

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-// button.addEventListener("click") {}
+let total = 0;
+let label = document.getElementById("label")
+let totalScore = document.getElementById("score");
+
+
+
 
 function diceRoll() {
+
+    label.innerHTML = "your current score"
 
     let dice = Math.floor(Math.random() * 6) + 1;
     console.log(dice)
 
     if (dice == 1) {
         document.getElementById("diceImg").src = "./img/dice1.png";
-        score = 0
+        total = 0
     } else if (dice == 2) {
         document.getElementById('diceImg').src = "./img/dice2.png";
-        score += 2
+        total += 2
     } else if (dice == 3) {
         document.getElementById('diceImg').src = "./img/dice3.png";
-        score += 3
+        total += 3
     } else if (dice == 4) {
         document.getElementById('diceImg').src = "./img/dice4.png";
-        score += 4
+        total += 4
     } else if (dice == 5) {
         document.getElementById('diceImg').src = "./img/dice5.png";
-        score += 5
+        total += 5
     } else if (dice == 6) {
         document.getElementById('diceImg').src = "./img/dice6.png";
-        score += 6
+        total += 6
+    }
+    totalScore.innerHTML = total;
+
+    if (total >= 21) {
+        label.innerHTML = "you win";
+        total = 0;
+    } else if (total == 0) {
+        label.innerHTML = "you lost";
     }
 
-}
 
-if (score > 19) {
-
-    ("you win")
 }
